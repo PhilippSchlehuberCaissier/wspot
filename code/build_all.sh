436633f2b5d1a52b1056d4512c3f87877e2edae6
Philipp Schlehuber-Caissier <philipp@lrde.epita.fr>
2022-11-22 17:13:04 +0100

    fix sudo

--- code/build_all.sh
+++ code/build_all.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
+CMAKEOPT=${1:""}
+CONFOPT=${2:""}
+DOINSTALL=${3:"No"}
+
 git clone https://github.com/PhilippSchlehuberCaissier/tchecker.git
 cd tchecker
 git checkout energyBA
 mkdir build
 cd build
-cmake $1 ..
+cmake $CMAKEOPT ..
 make -j 8
 cd ../..
 
@@ -14,8 +18,11 @@ git clone https://gitlab.lrde.epita.fr/spot/spot.git
 cd spot
 git checkout sven/weighted
 autoreconf -vfi
-./configure $2
+./configure $CONFOPT
 make -j 8
-make install -j 8
-make mostlyclean
+if [[ $DOINSTALL == "Yes" ]]
+then
+  make install -j 8
+  make mostlyclean
+fi
 
