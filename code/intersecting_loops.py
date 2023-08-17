e4175fbf57f14951afc42839aceb57ad7ad0ffaf
rania.saadi <rania.saadi@epita.fr>
2023-08-17 14:10:59 +0200

    added bench1, test for intersecting loops

---
+++ code/intersecting_loops.py
@@ -0,0 +1,55 @@
+import spot, buddy 
+
+def bench1_(n, aut, s0, wup):
+    if n < 3:
+        s = aut.new_state()
+        idx = aut.new_edge(s0, s, buddy.bddtrue)
+        spot.set_weight(aut, idx, 1)
+        idx = aut.new_edge(s, s0, buddy.bddtrue)
+        spot.set_weight(aut, idx, 1)
+        return 
+    else:
+        tmp = s0
+
+        for _ in range(1, n):
+            s = aut.new_state()
+            idx = aut.new_edge(tmp, s, buddy.bddtrue)
+            spot.set_weight(aut, idx, -wup)
+            tmp = s
+            bench1_(n - 1, aut, s, wup)
+        
+        idx = aut.new_edge(s, s0, buddy.bddtrue, [0])
+        spot.set_weight(aut, idx, 0)
+
+
+def bench1(n, wup):
+    if n < 2:
+        return None
+    
+    if n == 2:
+        aut = spot.make_twa_graph()
+        aut.new_states(2)
+        aut.set_buchi()
+        
+        idx = aut.new_edge(0, 1, buddy.bddtrue)
+        spot.set_weight(aut, idx, 1)
+        
+        aut.new_edge(1, 0, buddy.bddtrue, [0])
+        spot.set_weight(aut, idx, 1)
+    
+        return aut
+    
+    aut = spot.make_twa_graph()
+    aut.new_states(n)
+    aut.set_buchi()
+
+    for i in range(1, n):
+        idx = aut.new_edge(i - 1, i, buddy.bddtrue)
+        spot.set_weight(aut, idx, -wup)
+        bench1_(n - 1, aut, i, wup)
+    
+    aut.new_edge(n - 1, 0, buddy.bddtrue, [0])
+    spot.set_weight(aut, idx, 0)
+    bench1_(n - 1, aut, 0, wup)
+
+    return aut
