1aa31233c3d27847661ff0f5265071d1f6d262de
rania.saadi <rania.saadi@epita.fr>
2023-08-22 13:49:41 +0200

    cycle with 3 states to be tested

--- code/intersecting_loops.py
+++ code/intersecting_loops.py
@@ -1,13 +1,6 @@
 import spot, buddy 
 
-"""
-Bench1 recursively creates cycles with -wup weights 
-backedges have a weight of 0 
-cycles with 2 states have 1 weights
-the buchi condition is on the backedges
-"""
-
-def bench1_(n, aut, s0, wup):
+def bench_(n, aut, s0, wup):
     if n < 3:
         s = aut.new_state()
         idx = aut.new_edge(s0, s, buddy.bddtrue)
@@ -15,100 +8,24 @@ def bench1_(n, aut, s0, wup):
         idx = aut.new_edge(s, s0, buddy.bddtrue)
         spot.set_weight(aut, idx, 1)
         return 
-    else:
-        tmp = s0
-
-        for _ in range(1, n):
-            s = aut.new_state()
-            idx = aut.new_edge(tmp, s, buddy.bddtrue)
-            spot.set_weight(aut, idx, -wup)
-            tmp = s
-            bench1_(n - 1, aut, s, wup)
-        
-        idx = aut.new_edge(s, s0, buddy.bddtrue, [0])
-        spot.set_weight(aut, idx, 0)
-
-
-def bench1(n, wup):
-    if n < 2:
-        return None
     
-    if n == 2:
-        aut = spot.make_twa_graph()
-        aut.new_states(2)
-        aut.set_buchi()
-        
-        idx = aut.new_edge(0, 1, buddy.bddtrue)
-        spot.set_weight(aut, idx, 1)
-        
-        aut.new_edge(1, 0, buddy.bddtrue, [0])
-        spot.set_weight(aut, idx, 1)
-    
-        return aut
-    
-    aut = spot.make_twa_graph()
-    aut.new_states(n)
-    aut.set_buchi()
-
-    for i in range(1, n):
-        idx = aut.new_edge(i - 1, i, buddy.bddtrue)
-        spot.set_weight(aut, idx, -wup)
-        bench1_(n - 1, aut, i, wup)
-    
-    aut.new_edge(n - 1, 0, buddy.bddtrue, [0])
+    tmp = aut.new_state()
+    idx = aut.new_edge(s0, tmp, buddy.bddtrue)
     spot.set_weight(aut, idx, 0)
-    bench1_(n - 1, aut, 0, wup)
+    bench_(n - 1, aut, tmp, wup)
 
-    return aut
-
-
-"""
-Bench2 recursively creates cycles with 0 weights 
-on cycles with 3 states, the weights are respectively 0, -wup, -wup
-cycles with 2 states have 1 weights  
-the buchi condition is on the cycles with 3 states
-"""
-
-def bench2_(n, aut, s0, wup):
-    if n < 3:
+    for _ in range(2, n):
         s = aut.new_state()
-        idx = aut.new_edge(s0, s, buddy.bddtrue)
-        spot.set_weight(aut, idx, 1)
-        idx = aut.new_edge(s, s0, buddy.bddtrue)
-        spot.set_weight(aut, idx, 1)
-        return 
-    
-    if n == 3:
-        s = aut.new_state()
-        idx = aut.new_edge(s0, s, buddy.bddtrue)
-        spot.set_weight(aut, idx, 0)
-        bench2_(2, aut, s, wup)
-
+        idx = aut.new_edge(tmp, s, buddy.bddtrue)
+        spot.set_weight(aut, idx, -wup)
         tmp = s
-        for _ in range(2,n):
-            s = aut.new_state()
-            idx = aut.new_edge(tmp, s, buddy.bddtrue)
-            spot.set_weight(aut, idx, -wup)
-            tmp = s 
-            bench2_(2, aut, s, wup)
+        bench_(n - 1, aut, s, wup)
 
-        idx = aut.new_edge(s, s0, buddy.bddtrue, [0])
-        spot.set_weight(aut, idx, -wup)
-
-    else:
-        tmp = s0
-        for _ in range(1, n):
-            s = aut.new_state()
-            idx = aut.new_edge(tmp, s, buddy.bddtrue)
-            spot.set_weight(aut, idx, 0)
-            tmp = s
-            bench2_(n - 1, aut, s, wup)
-
-        idx = aut.new_edge(s, s0, buddy.bddtrue)
-        spot.set_weight(aut, idx, 0)
+    idx = aut.new_edge(s, s0, buddy.bddtrue)
+    spot.set_weight(aut, idx, 0)
 
 
-def bench2(n, wup):
+def bench(n, wup):
     if n < 2:
         return None
     
@@ -129,21 +46,23 @@ def bench2(n, wup):
     aut.new_states(n)
     aut.set_buchi()
 
-    w1 = 0    
+    idx = aut.new_edge(0, 1, buddy.bddtrue)
     if n == 3:
-        w1 = -3
-
-    for i in range(1, n):
-        idx = aut.new_edge(i - 1, i, buddy.bddtrue)
-        spot.set_weight(aut, idx, w1)
-        bench2_(n - 1, aut, i, wup)
-    
-    if n == 3:
-        aut.new_edge(n - 1, 0, buddy.bddtrue, [0])
+        spot.set_weight(aut, idx, -wup)
     else:
-        aut.new_edge(n - 1, 0, buddy.bddtrue)
+        spot.set_weight(aut, idx, 0)
+    bench_(n - 1, aut, 1, wup)
 
-    spot.set_weight(aut, idx, 0)
-    bench2_(n - 1, aut, 0, wup)
+    for i in range(2, n):
+        idx = aut.new_edge(i - 1, i, buddy.bddtrue)
+        spot.set_weight(aut, idx, -wup)
+        bench_(n - 1, aut, i, wup)
+
+    idx = aut.new_edge(n - 1, 0, buddy.bddtrue, [0])
+    if n == 3:
+        spot.set_weight(aut, idx, -wup)
+    else:
+        spot.set_weight(aut, idx, 0)
+    bench_(n - 1, aut, 0, wup)
 
     return aut
