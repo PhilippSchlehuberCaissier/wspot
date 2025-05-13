63f3a068af4cf002250e2bbb9d41816484342e67
Ugo Thay <ugo.thay@telecom-sudparis.eu>
2025-05-13 08:46:41 +0200

    Add larger test automaton

--- code/loop_benchmark.py
+++ code/loop_benchmark.py
@@ -3,15 +3,15 @@
 import spot, buddy, WBA_utils as wu
 import time
 
-hoa = spot.automaton("../tests/many_iterations_co_buechi_flattened.hoa")
+hoa = spot.automaton("../tests/large_co_buechi_flattened.hoa")
 
 print("Solving using the new algorithm")
 start = time.time()
-wu.OmegaEnergy(hoa, 0, 5, 0, 0)
+wu.OmegaEnergy(hoa, 0, 10, 0, 0)
 print(f"Time: {time.time() - start}")
 
 print("======================================")
 print("Solving using the legacy algorithm")
 start = time.time()
-wu.LEGACY_CoBuechiEnergy(hoa, 0, 5, 0)
+wu.LEGACY_CoBuechiEnergy(hoa, 0, 10, 0)
 print(f"Time: {time.time() - start}")
