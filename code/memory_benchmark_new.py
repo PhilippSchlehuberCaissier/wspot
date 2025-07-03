677d266623827ba33ca4a996c4bbc3c61ac904cd
Ugo Thay <ugo.thay@telecom-sudparis.eu>
2025-07-03 10:10:56 +0200

    Add memory benchmark

--- code/memory_benchmark_new.py
+++ code/memory_benchmark_new.py
@@ -1,9 +1,43 @@
 ## Benchmark to check the memory used by the new (backtrack) co-Büchi algorithm.
 
 import spot, buddy, WBA_utils as wu, WBA_solvers as ws
+import subprocess
+from memory_profiler import memory_usage
+import numpy as np
+import matplotlib.pyplot as plt
 
-this = "../tests/nested_loops.hoa"
-hoa = spot.automaton(this)
 
-for _ in range(100):
-    ws.NaiveCoBuechi(hoa, 0, 10, 0)
+solvers = [ws.NaiveCoBuechi, ws.LEGACY_CoBuechiEnergy, ws.CoBuechiEnergy]#, ws.CoBuechi_FW_new]
+memory = [[] for _ in range(len(solvers))]
+
+def to_benchmark(what):
+    subprocess.run(["python3", "nested_loops_builder.py", str(99)])
+    this = "../tests/nested_loops_auto.hoa"
+    hoa = spot.automaton(this)
+
+    what(hoa, 0, 10, 0)
+    del hoa
+    # ws.NaiveCoBuechi(hoa, 0, 10, 0)
+    # ws.LEGACY_CoBuechiEnergy(hoa, 0, 10, 0)
+    # ws.CoBuechiEnergy(hoa, 0, 10, 0)
+    # ws.CoBuechi_FW_new(hoa, 0, 10, 0)
+
+# if __name__ == '__main__':
+#     to_benchmark()
+
+
+for i in range(len(solvers)):
+    solver = solvers[i]
+    memory[i] = memory_usage((to_benchmark, (solver,)))
+
+n = max([len(mem) for mem in memory])
+for i in range(len(memory)):
+    memory[i] = np.pad(memory[i], (0, n-len(memory[i])), 'constant', constant_values=np.nan)
+
+time = np.linspace(0, n * 0.2, n)
+plt.plot(time, np.transpose(memory))
+plt.legend(["naive", "cycle storage", "backtracking"], loc="upper right")
+plt.title("Co-Büchi solving in the nested loops automaton")
+plt.xlabel("Execution time in s")
+plt.ylabel("Memory usage in MiB")
+plt.show()
